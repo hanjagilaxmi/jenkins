67148b925e49fe1b86cc3c7c60d1990c0939950b
deepa <deepa@DESKTOP-5Q6M2D1>
2023-08-27 16:53:18 +0530

    jen

---
+++ Formavenjenkins/src/formaven/Runner.java
@@ -0,0 +1,19 @@
+package formaven;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Runner {
+	@Test
+	public void script() {
+		System.setProperty("webdriver.chrome.driver","./Drivers/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://www.facebook.com");
+		driver.close();
+	}
+}
